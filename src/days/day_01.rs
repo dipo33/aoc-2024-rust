0889eea24129372fefb11fb3e6cb8d6c1a367f1f
dipo33 <mato.dipcar@gmail.com>
2024-12-01 21:50:58 +0100

    feat(01): implement

--- src/days/day_01.rs
+++ src/days/day_01.rs
@@ -1,18 +1,54 @@
-use aoc_common::day::{Day, Placeholder};
+use std::collections::HashMap;
+
+use aoc_common::day::Day;
 
 #[derive(Default)]
 pub struct Day01 {}
 
-impl Day for Day01 {
-    type P1 = Placeholder;
-    type P2 = Placeholder;
+fn parse_line(line: &str) -> (u32, u32) {
+    let (left, right) = line
+        .split_once("   ")
+        .expect("each line should contain two integers delimited by three spaces");
 
-    fn solve_part_1(&self, _input: &str) -> Self::P1 {
-        Placeholder
+    (
+        left.parse()
+            .expect("left column should contain only valid integers"),
+        right
+            .parse()
+            .expect("right column should contain only valid integers"),
+    )
+}
+
+impl Day for Day01 {
+    type P1 = u32;
+    type P2 = u32;
+
+    fn solve_part_1(&self, input: &str) -> Self::P1 {
+        let (mut left_items, mut right_items): (Vec<_>, Vec<_>) =
+            input.lines().map(parse_line).unzip();
+
+        left_items.sort_unstable();
+        right_items.sort_unstable();
+
+        left_items
+            .iter()
+            .zip(right_items.iter())
+            .map(|(left, &right)| left.abs_diff(right))
+            .sum()
     }
 
-    fn solve_part_2(&self, _input: &str) -> Self::P2 {
-        Placeholder
+    fn solve_part_2(&self, input: &str) -> Self::P2 {
+        let (left_items, right_items): (Vec<_>, Vec<_>) = input.lines().map(parse_line).unzip();
+
+        let mut frequencies = HashMap::new();
+        for num in right_items {
+            *frequencies.entry(num).or_insert(0) += 1;
+        }
+
+        left_items
+            .iter()
+            .map(|val| frequencies.get(val).copied().unwrap_or(0) * val)
+            .sum()
     }
 
     fn get_day(&self) -> u8 {
@@ -26,24 +62,32 @@ mod tests {
     use super::*;
 
     #[test]
-    #[ignore = "not yet implemented"]
     fn test_example_part_1() {
-        let input = r#""#;
+        let input = r#"3   4
+4   3
+2   5
+1   3
+3   9
+3   3"#;
 
         let day = Day01::default();
         let part_1 = day.solve_part_1(&input);
 
-        assert_eq!(part_1, Placeholder)
+        assert_eq!(part_1, 11)
     }
 
     #[test]
-    #[ignore = "not yet implemented"]
     fn test_example_part_2() {
-        let input = r#""#;
+        let input = r#"3   4
+4   3
+2   5
+1   3
+3   9
+3   3"#;
 
         let day = Day01::default();
         let part_2 = day.solve_part_2(&input);
 
-        assert_eq!(part_2, Placeholder)
+        assert_eq!(part_2, 31)
     }
 }

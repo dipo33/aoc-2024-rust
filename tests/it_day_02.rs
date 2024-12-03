435d2d61d3c3473e0b633ab082a4d2767ac8da31
dipo33 <mato.dipcar@gmail.com>
2024-12-03 03:32:56 +0100

    feat(02): implement

--- tests/it_day_02.rs
+++ tests/it_day_02.rs
@@ -5,15 +5,13 @@ use aoc_common::day::Day;
 use it_day::{test_part_1_uni, test_part_2_uni};
 
 #[test]
-#[ignore = "not yet implemented"]
 fn day02_part_1() {
-    let expected: <Day02 as Day>::P1 = <Day02 as Day>::P1::default();
+    let expected: <Day02 as Day>::P1 = 314;
     test_part_1_uni::<Day02>(expected);
 }
 
 #[test]
-#[ignore = "not yet implemented"]
 fn day02_part_2() {
-    let expected: <Day02 as Day>::P2 = <Day02 as Day>::P2::default();
+    let expected: <Day02 as Day>::P2 = 373;
     test_part_2_uni::<Day02>(expected);
 }
